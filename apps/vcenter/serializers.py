a010bf7066d9f0199a8d3b96a7e30e8b1ab803a1
elahe shokri <e.shokri@asax.ir>
2024-03-03 13:53:41 +0330

    add update endpoint and serializer

--- apps/vcenter/serializers.py
+++ apps/vcenter/serializers.py
@@ -5,7 +5,7 @@ class CreateVMSerializer(serializers.Serializer):
     vsphere_user = serializers.CharField()
     vsphere_password = serializers.CharField()
     vsphere_vcenter = serializers.CharField()
-    # vsphere_unverified_ssl = serializers.BooleanField()
+
     vsphere_datacenter = serializers.CharField()
     vsphere_cluster = serializers.CharField()
     vm_datastore = serializers.CharField()
@@ -17,3 +17,21 @@ class CreateVMSerializer(serializers.Serializer):
     vm_network_adapter_type = serializers.CharField()
     vm_disk_size = serializers.IntegerField()
     vm_disk_label = serializers.CharField()
+
+
+class UpdateVMSerializer(serializers.Serializer):
+    vsphere_user = serializers.CharField()
+    vsphere_password = serializers.CharField()
+    vsphere_vcenter = serializers.CharField()
+
+    vsphere_datacenter = serializers.CharField(required=False)
+    vsphere_cluster = serializers.CharField(required=False)
+    vm_datastore = serializers.CharField(required=False)
+    vm_network = serializers.CharField(required=False)
+    vm_name = serializers.CharField()
+    vm_cpu = serializers.IntegerField(required=False)
+    vm_ram = serializers.IntegerField(required=False)
+    vm_guest = serializers.CharField(required=False)
+    vm_network_adapter_type = serializers.CharField(required=False)
+    vm_disk_size = serializers.IntegerField(required=False)
+    vm_disk_label = serializers.CharField(required=False)
--- apps/vcenter/views.py
+++ apps/vcenter/views.py
@@ -1,8 +1,8 @@
 from rest_framework import views, response
 
-from apps.vcenter.serializers import CreateVMSerializer
+from apps.vcenter.serializers import CreateVMSerializer, UpdateVMSerializer
 from common.configs import TerraformConf
-from common.utilities import render_template, create_terraform_module, apply_terraform_module
+from common.utilities import render_template, create_terraform_module, apply_terraform_module, get_module_path
 
 
 class CreateVMView(views.APIView):
@@ -14,5 +14,19 @@ class CreateVMView(views.APIView):
             render_template(TerraformConf.template_path, terraform_vars,
                             f'{module_path}/terraform.tfvars')
             tf_result = apply_terraform_module(module_path)
+            # handle tf_result status code for errors in apply
+            return response.Response({'terraform_result': tf_result})
+        return response.Response(serializer.errors, status=400)
+
+    def put(self, request, vm_name):
+
+        serializer = UpdateVMSerializer(data=request.data)
+        if serializer.is_valid():
+            terraform_vars = serializer.validated_data
+            module_path = get_module_path(terraform_vars['vm_name'])
+            render_template(TerraformConf.template_path, terraform_vars,
+                            f'{module_path}/terraform.tfvars')
+            tf_result = apply_terraform_module(module_path)
+            # handle tf_result status code for errors in apply
             return response.Response({'terraform_result': tf_result})
         return response.Response(serializer.errors, status=400)
