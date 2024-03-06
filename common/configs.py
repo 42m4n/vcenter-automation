3b19aba3f87bef8462561f0d551b6f590d17c1cc
elahe shokri <e.shokri@asax.ir>
2024-03-06 10:37:19 +0330

    fix terraform config

--- common/configs.py
+++ common/configs.py
@@ -3,7 +3,7 @@ class Path:
 
 
 class TerraformConf:
-    base_init_path = './terraform1/'
+    base_init_path = './terraform/'
     template_path = 'terraform/terraform.tfvars.j2'
     variables_path = 'terraform/terraform.tfvars'
     binary_path = ".terraform/providers/registry.terraform.io/hashicorp/vsphere/2.1.1/linux_amd64/terraform-provider-vsphere"
