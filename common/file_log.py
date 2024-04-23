0d013e0cad897c3ab6689983caec7a4cf3958f01
elahe shokri <e.shokri@asax.ir>
2024-04-23 12:15:54 +0330

    add file log setting

---
+++ common/file_log.py
@@ -0,0 +1,20 @@
+from loguru import logger
+import logging
+
+
+class DjangoLogHandler(logging.Handler):
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            logger.opt(depth=1, exception=None).log(record.levelno, msg)
+        except Exception:
+            self.handleError(record)
+
+
+log_format = "<green>{time:YYYY-MM-DD--HH:mm:ss}</green> [{level}] - [{name} > {function}() > {line}] - {message}"
+
+# Add the custom handler to Django's logging configuration
+logging.basicConfig(handlers=[DjangoLogHandler()], level=logging.INFO)
+
+# Configure Loguru to write logs to files with rotation
+logger.add("logs/{time:YYYY-MM-DD--HH-DD}.log", format=log_format, rotation="1 day", level="INFO")
